c73b924ee8abd1a76fa7593e9eea7ff6442e9057
sebabitar <slbitar@uc.cl>
2018-08-19 15:54:54 -0300

    f

--- Programa/src/solver/main.c
+++ Programa/src/solver/main.c
@@ -10,16 +10,16 @@ int main()
 	/* Esta linea indica que el programa termino sin errores*/
 	push(stack, 1);
 	push(stack, 4);
-
+	push(stack, 2);
 
 	printf("%d\n", stack->first->color);
 	printf("%d\n", stack->first->next->color);
-	/*
+
 	int a = pop(stack);
 
 	printf("%d\n", a);
 	printf("%d\n", stack->size);
-
+	/*
 	int b = pop(stack);
 
 	printf("%d\n", b);
