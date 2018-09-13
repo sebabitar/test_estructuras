cfbdded575ab06f37ed69dcaa1082b11b46981d6
sebabitar <slbitar@uc.cl>
2018-09-12 22:07:44 -0300

    t2

--- Programa/src/autocomplete/trie.c
+++ Programa/src/autocomplete/trie.c
@@ -107,6 +107,7 @@ char * search(Trie *root, char *word)
           if (k == 26){
             end = true;
             letra[m+1] = '\0';
+            printf("%s\n", letra);
             strcat(word, letra);
             break;
           }
