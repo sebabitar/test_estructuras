49520971e9d833e1618e9a9131d9148c792636b8
sebabitar <slbitar@uc.cl>
2018-09-14 11:44:07 -0300

    t2

--- Programa/src/autocomplete/main.c
+++ Programa/src/autocomplete/main.c
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 		insert(trie, chars, freq);
 	}
 
-	
+	/*
 	int m;
 	fscanf(queries, "%d", &m);
 
@@ -67,7 +67,7 @@ int main(int argc, char *argv[])
 		}
 		fprintf(output, "%s\n", respuesta);
 	}
-
+*/
 	destroy(trie);
 	fclose(database);
 	fclose(queries);
